f183926a0079363bb26f3655be6ce4dd664bdb47
Ali Mozhdehi Fard <102677728+Alifarrd@users.noreply.github.com>
2022-08-24 10:25:55 +0430

    update

--- cattell16pf.py
+++ cattell16pf.py
@@ -1,22 +1,30 @@
 from cmath import sqrt
 import csv
 import math
-user=[10,10,10,10,11,10,10,10,10,10,10,10,10,10,10,10]
+user=[]
 re=[]
-c=0
 data=[]
+with open('input.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    for row in csv_reader:
+        user.append(row)
 with open('data.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     for row in csv_reader:
         data.append(row)
 for j in range (1,51):
+    c=0
     for i in range (0,16):
         b=int(data[j][i])
-        dis=(user[i]-b)*(user[i]-b)
+        a=int(user[1][i])
+        dis=(a-b)*(a-b)
         c=c+dis
-        c=math.sqrt(c)
-    re.append(c)
-re.sort()
+    d=math.sqrt(c)
+    re.append(d)
 
-for k in range(0,10):
-    print(re[k])
+
+re.sort()
+#for k in range(0,10):
+#    print(re[k])
+
+print(re)
