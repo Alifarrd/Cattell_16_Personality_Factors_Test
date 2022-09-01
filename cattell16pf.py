6394b386390c8ffd9eee8a9043a8501ad1ac9def
Ali Mozhdehi Fard <102677728+Alifarrd@users.noreply.github.com>
2022-09-02 02:44:06 +0430

    Update cattell16pf.py

--- cattell16pf.py
+++ cattell16pf.py
@@ -1,6 +1,9 @@
 from cmath import sqrt
 import csv
 import math
+from typing import Any
+import matplotlib.pyplot as plt
+import numpy as np
 user=[]
 data=[]
 with open('input.csv') as csv_file:
@@ -11,6 +14,7 @@ with open('data.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     for row in csv_reader:
         data.append(row)
+x=data[0][1:]
 data.pop(0)
 user.pop(0)
 l=len(data)
@@ -29,3 +33,20 @@ data.sort(key=lambda x: x[(17)])
 
 for i in range(0,9):
     print (data[i][0])
+
+
+y2=[]
+y4=[]
+y = data[1][1:17]
+y3=user[0][1:17]
+
+for i in y:
+    i=int(i)
+    y2.append(i)
+for i in y3:
+    i=int(i)
+    y4.append(i)
+plt.plot(x, y2, label="best mate")
+plt.plot(x, y4, label="user")
+plt.legend()
+plt.show()
