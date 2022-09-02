a99d6c36dd1aa3dce97c30a67df6bfa18db55b53
Ali Mozhdehi Fard <102677728+Alifarrd@users.noreply.github.com>
2022-09-02 23:28:02 +0430

    Update cattell16pf.py

--- cattell16pf.py
+++ cattell16pf.py
@@ -12,10 +12,13 @@ with open('data.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     for row in csv_reader:
         data.append(row)
+ 
 x=data[0][1:]
 lab.append(user[1][0])
 data.pop(0)
 user.pop(0)
+yplot=[]
+yuser=user[0][1:17]
 l=len(data)
 
 for j in range (0,l):
@@ -33,19 +36,17 @@ print ("Closest users are as follows:")
 for i in range(0,10):
     print (i+1,data[i][0],' ','User Grade:',data[i][17])
 
-yplot=[]
-yfirst = data[0][1:17]
-yuser=user[0][1:17]
-y=[yuser,yfirst]
-lab.append(data[0][0])
-c=0
-for i in y:
-    name=lab[c]
-    c+=1
-    yplot.clear()
-    for j in i:
-        j=int(j)
-        yplot.append(j)
+
+for n in range(0,3):
+    yfirst = data[n][1:17]
+    y=[yuser,yfirst]
+    lab.append(data[n][0])
+    for i in y:
+        name=lab[n]
+        yplot.clear()
+        for j in i:
+            j=int(j)
+            yplot.append(j)
     plt.plot(x, yplot, label=name)
 plt.legend()
 plt.show()
